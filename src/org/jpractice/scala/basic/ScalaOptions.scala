6b08f5e43973a3ce3fb28c824249cdf0b05d6b22
jenkins <1067031954@qq.com>
2022-05-23 00:04:03 +0800

    add scala option demo

---
+++ src/org/jpractice/scala/basic/ScalaOptions.scala
@@ -0,0 +1,23 @@
+package org.jpractice.scala.basic
+
+object ScalaOptions {
+
+  val lst = List(1,2,3)
+  val map = Map(1 -> "xf",2->"my",3->"xyr")
+
+  val opt : Option[Int] = Some(5)
+
+  def main(args: Array[String]): Unit = {
+
+    println(lst.find(_ > 2).getOrElse(0))
+
+    println(map.get(5).getOrElse("mgf"))
+
+    println(opt.isEmpty)
+
+    println(opt.get)
+
+
+  }
+
+}
