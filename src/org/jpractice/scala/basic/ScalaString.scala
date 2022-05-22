6f48bdf93bd16aa379230b18fa7e281f6942da07
jenkins <1067031954@qq.com>
2022-05-22 10:18:30 +0800

    add str demo

--- src/org/jpractice/scala/basic/ScalaString.scala
+++ src/org/jpractice/scala/basic/ScalaString.scala
@@ -2,7 +2,29 @@ package org.jpractice.scala.basic
 
 object ScalaString {
 
+
+  val num1 = 75
+  val num2 = 100.25
+  val str1 : String = "Hello World"
+  val str2 : String = "Max"
+
+
+
   def main(args: Array[String]): Unit = {
+
+    println(str1.length)
+    println(str1.concat(str2))
+    println(str1 + str2)
+
+    val result = printf("(%d -- %f -- %s)",num1,num2,str1)
+    println(result)
+    println("(%d -- %f -- %s)".format(num1,num2,str1))
+    printf("(%d -- %f -- %s)",num1,num2,str1)
+
+
+
+
+
     val name = "xuefei"
     val age = 32
     val salary = 123.3
