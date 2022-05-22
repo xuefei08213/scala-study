77a2d104f1f47382de3d6aa54eac8ed90df51da0
jenkins <1067031954@qq.com>
2022-05-22 19:56:37 +0800

    add scala map demo

---
+++ src/org/jpractice/scala/basic/ScalaMap.scala
@@ -0,0 +1,36 @@
+package org.jpractice.scala.basic
+
+object ScalaMap {
+
+  val myMap : Map[Int,String] = Map(1 -> "xf",2->"my",3->"xyr",3->"xyr1")
+
+  val myMap2 : Map[Int,String] = Map(5 -> "zz",6 -> "yc")
+
+  def main(args: Array[String]): Unit = {
+
+    println(myMap)
+
+    println(myMap(1))
+
+    println(myMap.keys)
+
+    println(myMap.values)
+
+    println(myMap.isEmpty)
+
+    myMap.keys.foreach{key => {
+      println("key" + key)
+      println("value" + myMap.get(key))
+    }}
+
+    println(myMap.contains(4))
+
+    println(myMap ++ myMap2)
+
+    println(myMap2.size)
+
+
+
+  }
+
+}
