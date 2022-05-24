57c1f3d139a1b6261004e2c1ddc046cc21d68539
jenkins <1067031954@qq.com>
2022-05-24 22:09:48 +0800

    add scala class extends demo and abstract class

--- src/org/jpractice/scala/oop/Polygon.scala
+++ src/org/jpractice/scala/oop/Polygon.scala
@@ -1,18 +1,18 @@
 package org.jpractice.scala.oop
 
-class Polygon {
+abstract class Polygon {
 
-  def area :Double = 0.0
+  def area :Double
 
 }
 
 object Polygon {
   def main(args: Array[String]): Unit = {
-    var poly = new Polygon
-
+//    var poly = new Polygon
+//    printArea(poly)
     var rectangle  = new Rectangle(55.2,10.2)
 
-    printArea(poly)
+
     printArea(rectangle)
 
     var tri =new Triangle(55.2,10.2)
